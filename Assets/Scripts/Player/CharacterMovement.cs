a8458e27f92ef2e6fb3deb59513684a8a95593c8
Martin Viidik <martinviidik@gmail.com>
2021-01-29 22:38:17 +0100

    Added URP pipeline, added player movement

---
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+
+    public Animator anim;
+    public SpriteRenderer sprite;
+
+    private Vector3 movementDirection;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        MoveCharacter();
+        Animate();
+    }
+
+    void HandleInput()
+    {
+        movementDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 15f);
+        Debug.Log(movementDirection);
+    }
+
+    void MoveCharacter()
+    {
+        rb.velocity = movementDirection * movementSpeed;
+    }
+
+    void Animate()
+    {
+        if(movementDirection != Vector3.zero)
+        {
+            anim.SetFloat("Horizontal", movementDirection.x);
+            anim.SetFloat("Vertical", movementDirection.z);
+        }
+        anim.SetFloat("Speed", movementSpeed);
+    }
+}
