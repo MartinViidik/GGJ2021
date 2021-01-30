037d1b3267a9fbeba90bba134d1b524bcc68310d
Marc Mehlitz <mmehlitz@gmail.com>
2021-01-31 00:05:25 +0100

    Making Sounds Playing

--- Assets/Scripts/Base/AdditionalStructs.cs
+++ Assets/Scripts/Base/AdditionalStructs.cs
@@ -9,3 +9,12 @@ public struct NamedInt
     public string name;
     public int value;
 }
+
+[Serializable]
+public struct NamedFmodSound
+{
+    public string name;
+
+    [FMODUnity.EventRef]
+    public string value;
+}
