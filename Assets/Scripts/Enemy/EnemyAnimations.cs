10974d21a9ab731c3e0bbfbf1615f5e7e0de587a
Martin Viidik <martinviidik@gmail.com>
2021-02-07 10:16:26 +0100

    Added dog stun animations

--- Assets/Scripts/Enemy/EnemyAnimations.cs
+++ Assets/Scripts/Enemy/EnemyAnimations.cs
@@ -1,11 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAnimations : MonoBehaviour
 {
     public EnemyAI Enemy;
     public Animator anim;
+    public SpriteRenderer sprite;
+
+    private void Start()
+    {
+        GameEvents.current.onStunEntity += OnStun;
+    }
+    private void OnDestroy()
+    {
+        GameEvents.current.onStunEntity -= OnStun;
+    }
+
+    private void OnStun(int interactableID, float stunPower)
+    {
+        if(Enemy.AgentVelocity().x <= 0)
+        {
+            anim.Play("StunLeft");
+        } else {
+            anim.Play("Stun");
+        }
+    }
     private void Update()
     {
         if (Enemy.AgentVelocity() != Vector3.zero && Enemy.AgentVelocity().magnitude > 2)
@@ -15,4 +33,5 @@ public class EnemyAnimations : MonoBehaviour
         }
         anim.SetFloat("Speed", Enemy.AgentVelocity().magnitude);
     }
+
 }
